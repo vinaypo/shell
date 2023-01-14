6d1a7932c7f2d9324fa85664b678fbafeefa5493
vinaypo <vapodila@gmail.com>
2023-01-14 18:35:43 +0500

    Modified the adv.sh

--- Shell/adv.sh
+++ Shell/adv.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-echo "Enter the disk usage: "
-read al
+
+backup_date=$(date +'%m/%d/%y %H:%M:%S')
 df -h | awk '{print $5 " " $1}' | while read op;
 do
   #echo "Disk Details: $op"
   avail=$(echo $op | awk '{print $1}'| cut -d'%' -f1)
   file_sys=$(echo $op | awk '{print $2}')
   #echo $avail
-  if (( $avail > $al ))
+  if [ $avail -ge 0 ]
   then
-	  echo "Critical by $file_sys!!"
+	  echo "Critical by $file_sys on $backup_date"
   fi
 done
 
